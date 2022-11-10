15c2d9fdebffdd96b0aa5e259c4231b6a61a22d8
Arif5577 <arifalamm33@gmail.com>
2022-11-10 08:53:00 +0600

    server done

--- server/utils/tourData.js
+++ server/utils/tourData.js
@@ -4,7 +4,7 @@ export const tourData = [
     price: 299,
     rating: 4.7,
     description:
-      "Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nIrure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      "Travelling Essay To travel from one place to another for different purposes is generally called travelling. People do not travel always for the same purposes. Someone travels to gain knowledge and experience, someone travels for pleasure, someone travels for business purposes etc. It has much educative value. It is a part of education. Our education and book knowledge remain imperfect without travelling. We can learn many things from travelling. It teaches us to trade and commerce, language, sociology, customs, culture, history, geography and so on. Therefore, the educative value of travelling beggars description. Besides this, there are some special benefits of travelling. If a man stays in one place for a long time, he becomes monotonous. Travelling removes our monotony and gives pleasure. It also broadens our outlook and refreshes our mind. A good traveller can easily educate others. He can give us first-hand knowledge of men and matters of another country. I like travelling very much.",
     image:
       "https://res.cloudinary.com/arif5577/image/upload/v1667972122/TRAVELIN/tour-2-1_sa4w2w.jpg",
   },
@@ -13,7 +13,7 @@ export const tourData = [
     price: 199,
     rating: 4.5,
     description:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Man is a traveler by nature. He does not like to keep static at a place for a long time. So, people like to visit new places whenever they find time and opportunity. People travel for different purposes. Some travel for getting an education and some travel for pleasure. Many people also travel for business and employment. Travelling is, indeed, very pleasant. It is useful too. By traveling to new places we can know new people and learn many new things. Travelling opens new vistas of experience and understanding for us. When we go to a new place all of our senses and instincts (বােধশক্তি) become awake (জাগ্রত). In such circumstances, we can learn new things very easily. Everything seems beautiful and sterling in a new place. With new scientific inventions, the ways and means of traveling have changed dramatically. Now different types of vehicles are available to us. They include bus, truck, train, minibus, lorry, microbus, maxi, airplane, helicopter, spacecraft, submarine, launch, steamer, hovercraft etc. By these vehicles, we can cover a long distance within a short time. So, we should try to travel more and more.",
     image:
       "https://res.cloudinary.com/arif5577/image/upload/v1667972124/TRAVELIN/tour-1-3_iskphe.jpg",
   },
@@ -22,7 +22,7 @@ export const tourData = [
     price: 399,
     rating: 4.2,
     description:
-      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum!\nDolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur, exercitation ullamco laboris nisi ut aliquip. Lorem ipsum dolor sit amet, consectetur adipisicing elit!",
+      "Travelling is, indeed, very pleasant. It is useful too. By traveling to new places we can know new people and learn many new things. Travelling opens new vistas of experience and understanding for us. When we go to a new place all of our senses (অনুভূতি) and instincts (বােধশক্তি) become awake (জাগ্রত), In such circumstances we can learn new things very easily. Everything seems beautiful and sterling at a new place. Purpose: People travels for different purposes. Some travel for getting an education and some travel for pleasure. Many people also travel for business and employment. In such circumstances we can learn new things very easily. Everything seems beautiful and sterling at a new place. Purpose: People travels for different purposes. Some travel for getting an education and some travel for pleasure. Many people also travel for business and employment.",
     image:
       "https://res.cloudinary.com/arif5577/image/upload/v1667972121/TRAVELIN/tour-9-cover_wwko4h.jpg",
   },
@@ -31,7 +31,7 @@ export const tourData = [
     price: 599,
     rating: 4.8,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum!",
+      "Travelling has manifold benefits. It is a part and parcel of our education. Our education and bookish knowledge remain imperfect without traveling. We may read about many places in our history books. If we can visit these places, the study of history becomes interesting. We read about men and things from different parts of the world. By traveling to different parts of the world, we see new people and new things. We can learn much about the manners and customs of those people. In this way, we can gain much knowledge. Consequently, our narrowness of mind disappears. Many people travel to different places to improve their health. If they go to a healthy place, they can recover their lost health. Travelling is also a source of joy to us. We see new objects and beautiful natural scenery. This refreshes both our mind and body.",
     image:
       "https://res.cloudinary.com/arif5577/image/upload/v1667972118/TRAVELIN/tour-8-2_trd0xp.jpg",
   },
@@ -40,7 +40,7 @@ export const tourData = [
     price: 99,
     rating: 4,
     description:
-      "Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nIrure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      " With new scientific inventions, the ways and means of traveling have changed dramatically. Now different types of vehicles are available to us. They include bus, truck, train, minibus, lorry, microbus, maxi, airplane, helicopter, spacecraft, submarine, launch, steamer, hovercraft etc. With these vehicles, we can cover a long distance within a short time. With new scientific inventions, the ways and means of traveling have changed dramatically. Now different types of vehicles are available to us. They include bus, truck, train, minibus, lorry, microbus, maxi, airplane, helicopter, spacecraft, submarine, launch, steamer, hovercraft etc. With these vehicles, we can cover a long distance within a short time.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     image:
       "https://res.cloudinary.com/arif5577/image/upload/v1667972110/TRAVELIN/tour-4-1_hcdqs9.jpg",
   },
@@ -49,7 +49,7 @@ export const tourData = [
     price: 599,
     rating: 4.6,
     description:
-      "Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nIrure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      "Generally, people do not travel always with the same purpose. Some people travel to gain knowledge, some go to healthy places to improve their health and some people travel for pleasure only. Businessmen travel for business purposes.Travelling has manifold benefits. It is a part and parcel of our education. Our education and bookish knowledge remain imperfect without traveling. We may read about many places in our history books. If we can visit these places, the study of history becomes interesting. We read about men and things from different parts of the world. By traveling to different parts of the world, we see new people and new things. We can learn much about the manners and customs of those people. In this way, we can gain much knowledge. Consequently, our narrowness of mind disappears. Many people travel to different places to improve their health. If they go to a healthy place, they can recover their lost health.",
     image:
       "https://res.cloudinary.com/arif5577/image/upload/v1667972109/TRAVELIN/tour-3-1_aid0xj.jpg",
   },
