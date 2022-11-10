4b5efbea17dac5c2e8068429156ea2fc6b667efb
Arif5577 <arifalamm33@gmail.com>
2022-11-10 19:13:38 +0600

    pagination added

--- server/controllers/tourControllers.js
+++ server/controllers/tourControllers.js
@@ -1,23 +1,27 @@
 import asyncHandler from "express-async-handler";
 import ReviewModel from "../model/ReviewModel.js";
 import TourModel from "../model/TourModel.js";
-import { uplaodImg } from "../utils/uploadImg.js";
 
 // PATH   --> /api/v1/tours
 // METHOD --> GET
 // ROUTE  --> PUBLIC
 
 export const getTours = asyncHandler(async (req, res) => {
-  const limtiData = req.query.limit;
-  const tours = await TourModel.find().limit(limtiData).sort({
-    createdAt: -1,
-  });
+  const limtiData = req.query.limit * 1;
+  const page = req.query.page * 1;
+  const totalService = await TourModel.countDocuments();
+  const tours = await TourModel.find()
+    .skip((page - 1) * limtiData)
+    .limit(limtiData)
+    .sort({
+      createdAt: -1,
+    });
 
   if (tours.length < 0) {
     res.status(500);
     throw new Error("Tours Not Found");
   }
-  res.status(200).json(tours);
+  res.status(200).json({ totalService, tours });
 });
 
 // PATH   --> /api/v1/tours/:id
@@ -42,7 +46,6 @@ export const getTourById = asyncHandler(async (req, res) => {
 
 export const createTour = asyncHandler(async (req, res) => {
   const { name, price, rating, description, image } = req.body;
-
   const newTour = new TourModel({
     name,
     price,
