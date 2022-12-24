fb2066eb16a0bdfba9e77f3c115728200b1b730a
Arif5577 <arifalamm33@gmail.com>
2022-12-24 12:37:12 +0600

    working with payment

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccount.json" assert { type: "json" };
+import serviceAccount from "./serviceAccount.json";
 
 const firebaseApp = admin.initializeApp({
   credential: admin.credential?.cert(serviceAccount),
